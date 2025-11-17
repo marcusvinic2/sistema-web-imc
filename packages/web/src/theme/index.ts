a10a0f00eb08b21fb36761f631dbe1b21920e9f4
Marcus Vinicius <marcusvinic.br.154@gmail.com>
2025-11-17 14:28:54 -0300

    configurar tema global do Chakra UI e modo de cor padrão

---
+++ packages/web/src/theme/index.ts
@@ -0,0 +1,18 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: false
+};
+
+export const theme = extendTheme({
+  config,
+  styles: {
+    global: {
+      body: {
+        bg: "gray.50",
+        color: "gray.800"
+      }
+    }
+  }
+});
